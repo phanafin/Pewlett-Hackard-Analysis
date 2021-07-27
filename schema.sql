cd7620b2f9dc43bb60a77defd0210c7639c6a22f
Patrick Hanafin <phanafin@comcast.net>
2021-07-26 19:09:44 -0600

    Through731

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
-	dept_no VARCHAR(4) NOT NULL,
-	dept_name VARCHAR(40) NOT NULL,
-	PRIMARY KEY (dept_no),
-	UNIQUE (dept_name)
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
 );
 
 CREATE TABLE employees (
@@ -35,22 +35,23 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_Emp (
-  emp_no INT NOT NULL,
-  dept_no VARCHAR(4) NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (dept_no)
+CREATE TABLE dept_emp (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE titles (
-  emp_no INT NOT NULL,
-  title VARCHAR(40) NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (title)
+	emp_no INT NOT NULL,
+	title VARCHAR(40) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (title, from_date)
 );
 
-SELECT * FROM departments;
+SELECT * FROM titles;
